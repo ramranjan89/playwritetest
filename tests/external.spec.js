9d7bd6544794a1c2932d05d2386d2de3aa625259
ramranjanjha89 <ramranjanjha89.rr@gmail.com>
2025-03-14 09:43:25 -0700

    I am printing on console

--- tests/external.spec.js
+++ tests/external.spec.js
@@ -6,5 +6,6 @@ test.only('automating UI', async ({page})=>
  await page.getByLabel("Email").fill("test11nov@test.com");
  await page.getByLabel("Password").fill("12345");
  await page.locator("input[value='Log in']").click();
+ console.log("Logged in successfully and clicked as well");
 
 })
